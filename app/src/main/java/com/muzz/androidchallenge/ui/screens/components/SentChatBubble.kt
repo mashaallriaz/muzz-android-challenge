df66874f44caf7e58f39a1d22203b5cb14f7610e
mashaallriaz <mashaallriaz@gmail.com>
2025-12-06 23:06:50 +0500

    Add read receipt icon on SentChatBubble

--- app/src/main/java/com/muzz/androidchallenge/ui/screens/components/SentChatBubble.kt
+++ app/src/main/java/com/muzz/androidchallenge/ui/screens/components/SentChatBubble.kt
@@ -6,12 +6,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.muzz.androidchallenge.R
 import com.muzz.androidchallenge.domain.models.Message
 import com.muzz.androidchallenge.ui.theme.MuzzColor
 import com.muzz.androidchallenge.ui.theme.MuzzSpacing
@@ -46,7 +51,17 @@ fun SentChatBubble(message: Message, isGrouped: Boolean) {
             Text(
                 text = message.text,
                 style = MuzzTypography.bodyLarge,
-                color = MuzzColor.White
+                color = MuzzColor.White,
+                modifier = Modifier.padding(end = MuzzSpacing.spacing24)
+            )
+
+            Icon(
+                painter = painterResource(id = R.drawable.ic_double_tick),
+                tint = MuzzColor.White,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(18.dp)
+                    .align(Alignment.BottomEnd)
             )
         }
     }
